5857e7bbcfc8a1b00cd425a6ca28b50b074de5bb
Abdulkhadarzilan <abdulcareers.ac@gmail.com>
2025-05-24 18:01:40 +0530

    Update vercel.json and vite.config.js for deployment config

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,3 @@
-// import path from "path"
-// import react from "@vitejs/plugin-react"
-// import { defineConfig } from "vite"
-
-// export default defineConfig({
-//   plugins: [react()],
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//     },
-//   },
-  
-// })
-
-
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
@@ -24,8 +9,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/", // <- Ensure base is set
-  build: {
-    outDir: "dist", // <- Vercel expects a static output folder
-  },
+  
 })
